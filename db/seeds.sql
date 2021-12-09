abb8365729995be4c424854a10d9b01b94dcc432
Gabriel Guerin <gjguerin96@gmail.com>
2021-12-08 17:02:10 -0800

    added addrole, addemployee, updaterole

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,12 +5,12 @@ VALUES  ("Accounting"),
         ("Research");
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ("Accountant", 60000, 1),
-        ("Quality Asssurance", 40000, 2),
-        ("Paralegal", 70000, 3),
-        ("Research Manager", 50000, 4),
-        ("Sales Representative", 60000, 2),
-        ("Head Lawyer", 90000, 3);
+VALUES  ("Accountant", "60000", 1),
+        ("Quality Asssurance", "40000", 2),
+        ("Paralegal", "70000", 3),
+        ("Research Manager", "50000", 4),
+        ("Sales Representative", "60000", 2),
+        ("Head Lawyer", "90000", 3);
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
 VALUES  ("Derek", "Payne", 1, NULL),
--- server.js
+++ server.js
@@ -10,37 +10,18 @@ const db = mysql.createConnection(
       database: 'employees_db'
     }
 );
-const myFunction = () => {
-    db.query('SELECT name FROM department', function (err, results) {
-        function getValues(item) {
-            return item.name
-        }
-        
-        return(results.map(getValues))
+
+const empListHelper = []
+
+const makeEmpList = () => {
+    db.query('SELECT last_name FROM employee', function (err, results) {
+        const lastNames = results.map(a => a.last_name)
+        empListHelper.push(lastNames)
+        // console.log(testArr[0])
     })
 }
-
-const deptList = ["Accounting","Sales","Legal","Research"];
-const roleList = ["Accountant","Sales Representative","Quality Assurance","Head Lawyer","Paralegal","Research Manager"]
-const roleQuestions = [
-    {
-        type:'input',
-        name: 'roleName',
-        message: 'Enter role name.'
-    },
-    {
-        type:'input',
-        name: 'roleSalary',
-        message: 'Enter role salary.'
-    },
-    {
-        type:'list',
-        name: 'roleDept',
-        message: 'Select role department.',
-        choices: deptList
-    },
-]
-
+makeEmpList()
+// const empList = empListHelper[0]
 
 function whatNext() {
     inquirer
@@ -49,10 +30,11 @@ function whatNext() {
                 type:'list',
                 name: 'next',
                 message: 'Choose an option.',
-                choices: ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role']
+                choices: ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role', 'Exit']
             }  
         ])
         .then((answer) => {
+            
             if (answer.next === 'View all departments'){
                 db.query('SELECT * FROM department', function (err, results) {
                     console.table(results);
@@ -60,6 +42,7 @@ function whatNext() {
                   });
                 
             }
+
             else if (answer.next === 'View all roles'){
                 const sql = 'SELECT role.title, role.salary, department.name FROM role LEFT JOIN department ON department.id = role.department_id;'
                 db.query(sql, function (err, results) {
@@ -67,14 +50,15 @@ function whatNext() {
                     whatNext()
                   });
             }
+
             else if (answer.next === 'View all employees'){
                 const sql = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON  role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;'
-                // const sql = 'SELECT * FROM employee'
                 db.query(sql, function (err, results) {
                     console.table(results);
                     whatNext()
                   });
             }
+
             else if (answer.next === 'Add a department'){
                 inquirer.prompt([
                     {
@@ -84,37 +68,143 @@ function whatNext() {
                     }  
                 ])
                 .then((answer) => {
-                    deptList.push(answer.newDept)
-                    // console.log(deptList)
-                    // const sql2 = 'INSERT INTO department (name) VALUES (answer.newDept)'; 
                                 
-                    db.query('INSERT INTO department (name) SET ?',answer.newDept, function (err, results) {
+                    db.query('INSERT INTO department (name) VALUES (?)',(answer.newDept), function (err, results) {
                         // console.table(results);
-                      });                        
+                      });
+                    console.log("Department added!")                        
                     whatNext()
                 })
             }
             else if (answer.next === 'Add a role'){
-                inquirer.prompt([
-                    roleQuestions[0],
-                    roleQuestions[1],
-                    roleQuestions[2]
-                ])
-                .then((answers) => {
-                    roleList.push(answer.roleDept)
-                    db.query('INSERT INTO role (title,salary) VALUES (?)(?)(?)',(answers.roleName,answers.roleSalary,answers.roleDept), function (err, results) {
-                        // console.table(results);
-                      });                        
-                    whatNext()
-                })
+                addRole()
             }
+
             else if (answer.next === 'Add an employee'){
-                return
+                addEmp()
             }
+
+            else  if (answer.next === 'Update an employee role'){
+                updateRole()
+            }
+
             else {
-                return
+                console.log("Goodbye!")
+                return;
             }
         });
+}
+
+function addRole(){
+    db.query('SELECT name FROM department', function (err, results) {
+        const deptList = results.map(a => a.name)
+
+        inquirer.prompt([
+            {
+                type:'input',
+                name: 'roleName',
+                message: 'Enter role name.'
+            },
+            {
+                type:'input',
+                name: 'roleSalary',
+                message: 'Enter role salary.'
+            },
+            {
+                type:'list',
+                name: 'roleDept',
+                message: 'Select role department.',
+                choices: deptList
+            }
+        ])
+
+        .then((answers) => {
+                const deptId = (deptList.indexOf(answers.roleDept)) + 1
+                // console.log(answers.roleName,answers.roleSalary,deptId)
+                db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)', [answers.roleName, answers.roleSalary, deptId], function (err, results) {
+                    // console.table(results);
+                }); 
+                console.log("Role added!")                        
+                whatNext()                       
+                
+            })
+    })  
+}
+
+
+function addEmp(){
+    db.query('SELECT title FROM role', function (err, results) {
+        const roleList = results.map(a => a.title)
+        const empList = empListHelper[0]
+
+        
+        inquirer.prompt([
+            {
+                type:'input',
+                name: 'firstName',
+                message: 'Enter employee first name.'
+            },
+            {
+                type:'input',
+                name: 'lastName',
+                message: 'Enter employee last name.'
+            },
+            {
+                type:'list',
+                name: 'empRole',
+                message: 'Select employee role.',
+                choices: roleList
+            },
+            {
+                type:'list',
+                name: 'empMang',
+                message: 'Select employee manager.',
+                choices: empList
+            }
+        ])
+
+        .then((answers) => {
+                const roleId = (roleList.indexOf(answers.empRole)) + 1
+                const mangId = (empList.indexOf(answers.empMang)) + 1
+
+                db.query('INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)',[answers.firstName,answers.lastName,roleId,mangId], function (err, results) {
+                    // console.table(results);
+                  });
+                console.log("Employee added!")                        
+                whatNext()
+            })
+    })  
+}
+
+function updateRole() {
+    db.query('SELECT title FROM role', function (err, results) {
+        const roleList = results.map(a => a.title)
+        const empList = empListHelper[0]
+
+        inquirer.prompt([
+            {
+                type:'list',
+                name: 'lastName',
+                message: 'Select an employee.',
+                choices: empList
+            },
+            {
+                type:'list',
+                name: 'empRole',
+                message: 'Select their new role.',
+                choices: roleList
+            }
+        ])
+        .then((answers) => {
+            const roleId = (roleList.indexOf(answers.empRole)) + 1
+
+            db.query('UPDATE employee SET role_id = ? WHERE last_name = ?', [roleId,answers.lastName], function (err, results) {
+                console.table(results);
+              });
+            console.log("Role updated!")                        
+            whatNext()
+        })
+    })    
 }
 
 whatNext()
